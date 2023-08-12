808a5c34c2806cec5d568fbe6087679ccc560cb8
israelGitV1 <kasper.war@gmail.com>
2023-08-12 13:14:52 -0300

    add method precoComDesconto in the class Produto

--- ex/src/classe/MainProduto.java
+++ ex/src/classe/MainProduto.java
@@ -17,8 +17,8 @@ public class MainProduto {
 		p2.preco = 12.56;
 	    p2.desconto = 0.29;
 		
-	    double precoFinal1 = p1.preco * (1-p1.desconto);
-	    double precoFinal2 = p2.preco * (1-p2.desconto);
+	    double precoFinal1 = p1.precoComDesconto();
+	    double precoFinal2 = p2.precoComDesconto(0.1);
 	    
 	    System.out.printf("Média do preço do %s e %s menos desconto R$: %.2f",p1.nome,p2.nome, (precoFinal1 + precoFinal2) / 2);
 		entrada.close();

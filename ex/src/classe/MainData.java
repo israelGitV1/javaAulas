1cc8a2285eed7d3fc3a054d1d052d5d5a013a1df
israelGitV1 <kasper.war@gmail.com>
2023-08-13 15:25:04 -0300

    add constructor in the class Data

--- ex/src/classe/MainData.java
+++ ex/src/classe/MainData.java
@@ -5,25 +5,25 @@ import java.util.Scanner;
 public class MainData {
 	public static void main(String[] args) {
 		var data1 = new Data();
-		Data data2 = new Data();
+		Data data2 = new Data(19,8,1995);
 		
-		Scanner entrada = new Scanner(System.in);
-		System.out.println("Digite primeira data: ");
-		System.out.print("Dia: ");
-		data1.dia = entrada.nextInt();
-		System.out.print("Mês: ");
-		data1.mes = entrada.nextInt();
-		System.out.print("Ano: ");
-		data1.ano = entrada.nextInt();
-		
-		System.out.println("Digite segunda data: ");
-		System.out.print("Dia: ");
-		data2.dia =entrada.nextInt();
-		System.out.print("Mês: ");
-		data2.mes =entrada.nextInt();
-		System.out.print("Ano: ");
-		data2.ano =entrada.nextInt();
-		entrada.close();
+//		Scanner entrada = new Scanner(System.in);
+//		System.out.println("Digite primeira data: ");
+//		System.out.print("Dia: ");
+//		data1.dia = entrada.nextInt();
+//		System.out.print("Mês: ");
+//		data1.mes = entrada.nextInt();
+//		System.out.print("Ano: ");
+//		data1.ano = entrada.nextInt();
+//		
+//		System.out.println("Digite segunda data: ");
+//		System.out.print("Dia: ");
+//		data2.dia =entrada.nextInt();
+//		System.out.print("Mês: ");
+//		data2.mes =entrada.nextInt();
+//		System.out.print("Ano: ");
+//		data2.ano =entrada.nextInt();
+//		entrada.close();
 		
 		System.out.printf(" Data1: %s \n Data2: %s",data1.obterDataFormatada(),
 				                                              data2.obterDataFormatada());

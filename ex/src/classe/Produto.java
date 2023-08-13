c4aed7f9f01276a90d6b2a3da2cfe39758e8736d
israelGitV1 <kasper.war@gmail.com>
2023-08-13 08:44:03 -0300

    add constructor in the class 'Produto'

--- ex/src/classe/Produto.java
+++ ex/src/classe/Produto.java
@@ -6,6 +6,15 @@ public class Produto {
 	double preco;
 	double desconto;
 	
+	Produto(){
+		
+	}
+	
+	Produto(String nomeInicial,double preco,double desconto){
+		nome=nomeInicial;
+		this.preco= preco;
+		this.desconto = desconto;
+	}
 	double precoComDesconto() {
 		return preco * (1 - desconto);
 	}

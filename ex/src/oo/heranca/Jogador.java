ef1adb5ef3ccad973e754b31686959a3f70e6444
israelGitV1 <kasper.war@gmail.com>
2023-09-21 10:40:50 -0300

    add heranca: jogador and jogo

--- ex/src/oo/heranca/Jogador.java
+++ ex/src/oo/heranca/Jogador.java
@@ -2,12 +2,30 @@ package oo.heranca;
 
 public class Jogador {
 
+	int vida = 100;
 	int x;
 	int y;
+	
+	boolean atacar(Jogador oponente){
+		
+		int deltaX = Math.abs(this.x - oponente.x);
+		int deltaY = Math.abs(this.y - oponente.y);
+		
+		if(deltaX == 0 && deltaY == 1 ) {
+			oponente.vida-=10;
+			return true;
+		}else if(deltaX == 1 && deltaY == 0) {
+			oponente.vida -= 10;
+			return true;
+		}else
+			return false;
+		
+	}
 	
 	boolean andar(Direcao direcao) {
 		
 		switch(direcao){
+		
 			case NORTE:
 				y--;
 				break;
@@ -23,4 +41,6 @@ public class Jogador {
 		}	 
 		return true;
 	}
+	
+	
 }
--- ex/src/oo/heranca/Jogo.java
+++ ex/src/oo/heranca/Jogo.java
@@ -4,16 +4,25 @@ public class Jogo {
 
 	public static void main(String[] args) {
 		Jogador j1 = new Jogador();
-		
 		j1.x = 10;
 		j1.y = 10;
 		
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
-		j1.andar(Direcao.NORTE);
-		j1.andar(Direcao.LESTE);
-
-		System.out.println(j1.y);
-		System.out.println(j1.x);
+		Jogador j2 = new Jogador();
+		j2.x = 10;
+		j2.y = 11;
+		
+		System.out.println(j1.vida);
+		System.out.println(j2.vida);
+		
+		j1.atacar(j2);
+		
+		System.out.println("\n"+j1.vida);
+		System.out.println(j2.vida);
+		
+		j2.atacar(j1);
+		
+		System.out.println("\n"+j1.vida);
+		System.out.println(j2.vida);
+		
 	}
 }

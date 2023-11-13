5647ba0618a0d1e3f06100dc16c78d4b743afb31
israelGitV1 <kasper.war@gmail.com>
2023-11-13 13:26:09 -0300

    Lambdas: Desafio

---
+++ ex/src/lambdas/Desafio.java
@@ -0,0 +1,52 @@
+package lambdas;
+
+import java.text.DecimalFormat;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Desafio {
+
+	public static void main(String[] args) {
+		
+		Produto  p1 = new Produto("iPad", 3235.89, 0.13);
+		DecimalFormat df = new DecimalFormat("#,###.00");
+		;
+		/*
+		 * 1. A partir do produto calculado o preco real (com desconto)
+		 * 2. Imposto Municipal: >= 2500 (8,5%)/ < 2500	(Isento)
+		 * 3. Frete: >= 3000 (100)/ < 3000 (50)
+		 * 4. Arredondar: Deixar duas casas decimais
+		 * 5. Formatar: R$1234,56
+		 * 
+		 */
+		
+		Predicate<Produto> isCaro =
+				prod -> (prod.preco * (1 - prod.desconto)) >= 750;
+		
+		Produto produto = new Produto("Notebook",3893.89 ,0.15);
+		System.out.println(isCaro.test(produto));
+		
+		
+		Function<Produto, Double> comDesconto = p -> p.preco * (1 - p.desconto);
+		Function<Double, Double> comImposto = v -> v >= 2500 ?(v * 0.085) + v : v ;
+		Function<Double, Double> comFrete = v -> v >= 3000 ? v + 100 : v + 50;
+		Function<Double, String> comArredondamento = v -> df.format(v);
+		Function<String, String> comFomatoReal = v -> "R$:" + v;
+		
+		System.out.println(comDesconto.apply(p1));
+		System.out.println(comDesconto.andThen(comImposto).apply(p1));
+		System.out.println(comDesconto.andThen(comImposto).andThen(comFrete).apply(p1));
+		System.out.println(comDesconto.andThen(comImposto)
+				                      .andThen(comFrete)
+				                      .andThen(comArredondamento)
+				                      .apply(p1));
+		System.out.println(comDesconto.andThen(comImposto)
+                .andThen(comFrete)
+                .andThen(comArredondamento)
+                .andThen(comFomatoReal)
+                .apply(p1));
+		
+		
+	
+	}
+}
